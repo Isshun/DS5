460c469b2b00d201b998cca16d98885b89111b5e
Alexis Lauper <alexis.lauper@gmail.com>
2013-12-22 19:08:05 +0100

    win

--- src/make.bat
+++ src/make.bat
@@ -1,1 +1,2 @@
-g++ ..\DS5\DS5\src\*.cpp --std=c++11 -I..\SFML-2.0\include -I..\DS5\DS5\src -L..\SFML-2.0\lib -lsfml-graphics -lsfml-window -lsfml-system -o ds5
+cd C:\MinGW\bin\
+g++ ..\DS5\DS5\src\*.cpp --std=c++11 -I..\SFML-2.0\include -I..\DS5\DS5\src -L..\SFML-2.0\lib -lsfml-graphics -lsfml-window -lsfml-system -o ..\DS5\DS5\bin\ds5.exe
